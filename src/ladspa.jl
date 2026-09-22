203e370485bd8ad599815f051e77015f9a7853a1
Florian Paul Schmidt <mista.tapas@gmx.net>
2026-09-22 14:19:02 +0200

    ok

--- src/ladspa.jl
+++ src/ladspa.jl
@@ -26,14 +26,29 @@ struct Descriptor
 end
 
 function load_library(path)
-    Libdl.LazyLibrary(path)
+    Libdl.dlopen(path)
 end
 
-function descriptor(path, index)
-    l = Libdl.dlopen(path)
-    f = Libdl.dlsym(l, "ladspa_descriptor")
-    d = ccall(f, Ptr{LADSPA.Descriptor}, (Int32,), 0)
-    unsafe_load(d)
+function descriptor(lib, index)
+    f = Libdl.dlsym(lib, "ladspa_descriptor")
+    d = ccall(f, Ptr{LADSPA.Descriptor}, (Int32,), index)
+    (d, unsafe_load(d))
+end
+
+function instantiate(descriptor, samplerate)
+    ccall(unsafe_load(descriptor).instantiate, Ptr{Cvoid}, (Ptr{Descriptor}, Culong), descriptor, samplerate)
+end
+
+function activate(descriptor, instance)
+    ccall(unsafe_load(descriptor).activate, Cvoid, (Ptr{Cvoid},), instance)
+end
+
+function connect_port(descriptor, instance, port_index, buffer)
+    ccall(unsafe_load(descriptor).connect_port, Cvoid, (Ptr{Cvoid}, Culong, Ptr{Float32}), instance, port_index, buffer)
+end
+
+function run(descriptor, instance, sample_count)
+    ccall(unsafe_load(descriptor).run, Cvoid, (Ptr{Cvoid}, Culong), instance, sample_count)
 end
 
 end
